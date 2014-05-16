65d42533af8380b1c3e2a8f52c52d24ce2ccf8a2
marinejt <jean.theo.vinet@gmail.com>
2014-05-16 10:40:21 -0400

    Base project

---
+++ GestionRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionRestaurant.Startup))]
+namespace GestionRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
